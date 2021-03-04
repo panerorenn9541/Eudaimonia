b0341bc5bff907d63537955f5e4d5ded1bbffde1
panerorenn9541 <36008213+panerorenn9541@users.noreply.github.com>
2021-03-04 10:11:41 -0800

    Add more to UI with images

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.clock import Clock
+from plyer import notification
 
 KV = '''
 <TitleScreen>:
@@ -91,40 +92,186 @@ KV = '''
             root.stats_enter()
 
 <DietScreen>:
-    restrictions: restrictions
-    goals:goals
     name: "diet"
     MDLabel:
-        text: "Let us know about your diet and goals"
+        text: "Let us know about your dietary restrictions"
         halign: "center"
         pos_hint:{'center_y': 0.9}
         font_style: "H4"
         markup: True
         bold: True
         
-    MDTextField:
-        id: restrictions
-        hint_text: "Enter any dietary restrictions you may have"
-        pos_hint: {'center_x': 0.5, 'center_y': 0.7}
-        size_hint_x: None
-        width: 300
-        required: True
-    
-    MDTextField:
-        id: goals
-        hint_text: "Enter your current goals"
-        pos_hint: {'center_x': 0.5, 'center_y': 0.5}
-        size_hint_x: None
-        width: 300
-        required: True
+    MDCheckbox:
+        pos_hint:{'center_x' : 0.2, 'center_y': 0.7}
+        size_hint: None, None
+        size: dp(70), dp(70)
+        on_active:
+            root.toggle_lactose()
+        
+    MDLabel:
+        text: "Lactose Intolerance"
+        halign: "center"
+        pos_hint:{'center_x' : 0.5, 'center_y': 0.7}
+        markup: True
+        bold: False
+        
+    MDCheckbox:
+        pos_hint:{'center_x' : 0.2, 'center_y': 0.6}
+        size_hint: None, None
+        size: dp(70), dp(70)
+        on_active:
+            root.toggle_vegan()
+        
+    MDLabel:
+        text: "Vegetarian or Vegan"
+        halign: "center"
+        pos_hint:{'center_x' : 0.5, 'center_y': 0.6}
+        markup: True
+        bold: False
+        
+    MDCheckbox:
+        pos_hint:{'center_x' : 0.2, 'center_y': 0.5}
+        size_hint: None, None
+        size: dp(70), dp(70)
+        on_active:
+            root.toggle_pescetarian()
+        
+    MDLabel:
+        text: "Pescetarian"
+        halign: "center"
+        pos_hint:{'center_x' : 0.5, 'center_y': 0.5}
+        markup: True
+        bold: False
+        
+    MDCheckbox:
+        pos_hint:{'center_x' : 0.2, 'center_y': 0.4}
+        size_hint: None, None
+        size: dp(70), dp(70)
+        on_active:
+            root.toggle_peanut()
+        
+    MDLabel:
+        text: "Peanut Allergies"
+        halign: "center"
+        pos_hint:{'center_x' : 0.5, 'center_y': 0.4}
+        markup: True
+        bold: False
+        
+    MDCheckbox:
+        pos_hint:{'center_x' : 0.2, 'center_y': 0.3}
+        size_hint: None, None
+        size: dp(70), dp(70)
+        on_active:
+            root.toggle_gluten()
+        
+    MDLabel:
+        text: "Gluten Intolerance"
+        halign: "center"
+        pos_hint:{'center_x' : 0.5, 'center_y': 0.3}
+        markup: True
+        bold: False
         
     MDRectangleFlatButton:
         text: "Done"
-        pos_hint:{'center_x': 0.5, 'center_y': 0.3}
+        pos_hint:{'center_x': 0.5, 'center_y': 0.1}
         on_press:
-            root.diet_enter()        
+            root.diet_enter()
 
+<GoalsScreen>:
+    name: "goals"
+    MDLabel:
+        text: "Let us know about some of your goals"
+        halign: "center"
+        pos_hint:{'center_y': 0.9}
+        font_style: "H4"
+        markup: True
+        bold: True
+        
+    MDCheckbox:
+        pos_hint:{'center_x' : 0.2, 'center_y': 0.7}
+        size_hint: None, None
+        size: dp(70), dp(70)
+        on_active:
+            root.toggle_lose_weight()
+        
+    MDLabel:
+        text: "Lose Weight"
+        halign: "center"
+        pos_hint:{'center_x' : 0.5, 'center_y': 0.7}
+        markup: True
+        bold: False
+        
+    MDCheckbox:
+        pos_hint:{'center_x' : 0.2, 'center_y': 0.6}
+        size_hint: None, None
+        size: dp(70), dp(70)
+        on_active:
+            root.toggle_gain_muscle()
+        
+    MDLabel:
+        text: "Gain Muscle"
+        halign: "center"
+        pos_hint:{'center_x' : 0.5, 'center_y': 0.6}
+        markup: True
+        bold: False
+        
+    MDCheckbox:
+        pos_hint:{'center_x' : 0.2, 'center_y': 0.5}
+        size_hint: None, None
+        size: dp(70), dp(70)
+        on_active:
+            root.toggle_wake_up()
+        
+    MDLabel:
+        text: "Wake Up Early"
+        halign: "center"
+        pos_hint:{'center_x' : 0.5, 'center_y': 0.5}
+        markup: True
+        bold: False
+        
+    MDCheckbox:
+        pos_hint:{'center_x' : 0.2, 'center_y': 0.4}
+        size_hint: None, None
+        size: dp(70), dp(70)
+        on_active:
+            root.toggle_plant_food()
+        
+    MDLabel:
+        text: "Eat More Plant Based Foods"
+        halign: "center"
+        pos_hint:{'center_x' : 0.5, 'center_y': 0.4}
+        markup: True
+        bold: False
+        
+    MDCheckbox:
+        pos_hint:{'center_x' : 0.2, 'center_y': 0.3}
+        size_hint: None, None
+        size: dp(70), dp(70)
+        on_active:
+            root.toggle_sugar()
+        
+    MDLabel:
+        text: "Cut Down On Sugar"
+        halign: "center"
+        pos_hint:{'center_x' : 0.5, 'center_y': 0.3}
+        markup: True
+        bold: False
+        
+    MDRectangleFlatButton:
+        text: "Done"
+        pos_hint:{'center_x': 0.5, 'center_y': 0.1}
+        on_press:
+            root.goals_enter()
             
+<WelcomeScreen>:
+    name: "welcome"
+    MDLabel:
+        text: "Welcome to Eudaimonia"
+        halign: "center"
+        font_style: "H2"
+        markup: True
+        bold: True
+
 <HomeScreen>:
     MDBottomNavigation:
         panel_color: .2, .2, .2, 1
@@ -140,10 +287,18 @@ KV = '''
                 font_style: "H4"
                 markup: True
                 bold: True
+            
+            Image:
+                source: 'images\eudaimoniaLogo.png'
+                pos_hint:{'center_y' : 0.4}
 
         MDBottomNavigationItem:
             name: "food"
             text: "Food"
+            
+            Image:
+                source: 'images\eplate.png'
+                pos_hint:{'center_y': 0.3}
 
             MDLabel:
                 text: "Food"
@@ -152,10 +307,22 @@ KV = '''
                 font_style: "H4"
                 markup: True
                 bold: True
+                
+            MDTextButton:
+                text: "Enter Your Meals ->"
+                pos_hint:{'center_x' : 0.9, 'center_y': 0.8}
+                on_press:
+                    root.enter_meals()
 
         MDBottomNavigationItem:
             name: "exercise"
             text: "Exercise"
+            
+            Image:
+                source: 'images\eweight.png'
+                pos_hint:{'center_y': 0.3, 'center_x': 0.5}
+                size_hint_x: 0.5
+                size_hint_y: 0.5
 
             MDLabel:
                 text: "Exercise"
@@ -164,10 +331,22 @@ KV = '''
                 font_style: "H4"
                 markup: True
                 bold: True
+                
+            MDTextButton:
+                text: "Enter Your Workouts ->"
+                pos_hint:{'center_x' : 0.88, 'center_y': 0.8}
+                on_press:
+                    root.enter_exercise()
 
         MDBottomNavigationItem:
             name: "sleep"
             text: "Sleep"
+            
+            Image:
+                source: 'images\ebed.png'
+                pos_hint:{'center_y': 0.3, 'center_x': 0.5}
+                size_hint_x: 0.8
+                size_hint_y: 0.8
 
             MDLabel:
                 text: "Sleep"
@@ -176,9 +355,16 @@ KV = '''
                 font_style: "H4"
                 markup: True
                 bold: True
+                
+            MDTextButton:
+                text: "Enter Your Sleep Times ->"
+                pos_hint:{'center_x' : 0.87, 'center_y': 0.8}
+                on_press:
+                    root.enter_sleep()
 
 <FoodScreen>:
-    name: "food"
+    meal: meal
+    name: "food_entry"
     MDLabel:
         text: "Food"
         pos_hint:{'center_y': 0.9}
@@ -187,8 +373,23 @@ KV = '''
         markup: True
         bold: True
         
+    MDTextField:
+        id: meal
+        hint_text: "Enter your meal"
+        pos_hint: {'center_x': 0.5, 'center_y': 0.5}
+        size_hint_x: None
+        width: 300
+        required: True
+
+    MDRectangleFlatButton:
+        text: "Done"
+        pos_hint:{'center_x': 0.5, 'center_y': 0.4}
+        on_press:
+            root.food_enter()
+        
 <ExerciseScreen>:
-    name: "exercise"
+    workout: workout
+    name: "exercise_entry"
     MDLabel:
         text: "Exercise"
         pos_hint:{'center_y': 0.9}
@@ -197,8 +398,24 @@ KV = '''
         markup: True
         bold: True
         
+    MDTextField:
+        id: workout
+        hint_text: "Enter your workout"
+        pos_hint: {'center_x': 0.5, 'center_y': 0.5}
+        size_hint_x: None
+        width: 300
+        required: True
+
+    MDRectangleFlatButton:
+        text: "Done"
+        pos_hint:{'center_x': 0.5, 'center_y': 0.4}
+        on_press:
+            root.exercise_enter()
+        
 <SleepScreen>:
-    name: "sleep"
+    sleep_start: sleep_start
+    sleep_end: sleep_end
+    name: "sleep_entry"
     MDLabel:
         text: "Sleep"
         pos_hint:{'center_y': 0.9}
@@ -206,6 +423,28 @@ KV = '''
         font_style: "H4"
         markup: True
         bold: True
+        
+    MDTextField:
+        id: sleep_start
+        hint_text: "Enter when you went to sleep"
+        pos_hint: {'center_x': 0.5, 'center_y': 0.7}
+        size_hint_x: None
+        width: 300
+        required: True
+        
+    MDTextField:
+        id: sleep_end
+        hint_text: "Enter when you woke up"
+        pos_hint: {'center_x': 0.5, 'center_y': 0.5}
+        size_hint_x: None
+        width: 300
+        required: True
+
+    MDRectangleFlatButton:
+        text: "Done"
+        pos_hint:{'center_x': 0.5, 'center_y': 0.3}
+        on_press:
+            root.sleep_enter()
 
 <Manager>:
     TitleScreen:
@@ -218,14 +457,18 @@ KV = '''
         name: "stats"
     DietScreen:
         name: "diet"
+    GoalsScreen:
+        name: "goals"
+    WelcomeScreen:
+        name: "welcome"
     HomeScreen:
         name: "home"
     FoodScreen:
-        name: "food"
+        name: "food_entry"
     ExerciseScreen:
-        name: "exercise"
+        name: "exercise_entry"
     SleepScreen:
-        name: "sleep"
+        name: "sleep_entry"
 '''
 
 Builder.load_string(KV)
@@ -267,25 +510,81 @@ class StatsScreen(Screen):
 
 class DietScreen(Screen):
     def diet_enter(self):
-        self.restrictions = self.restrictions.text
-        self.goals = self.goals.text
+        self.parent.current = "goals"
+
+    def toggle_vegan(self):
+        '''***NEED TO CHANGE TO A TOGGLE INSTEAD OF SETTING TRUE***  MAKE A USER CLASS'''
+        self.vegan = True
+
+    def toggle_lactose(self):
+        self.lactose = True
+
+    def toggle_peanut(self):
+        self.peanut = True
+
+    def toggle_gluten(self):
+        self.gluten = True
+
+    def toggle_pescetarian(self):
+        self.pescetarian = True
+
+
+class GoalsScreen(Screen):
+    def goals_enter(self):
+        self.parent.current = "welcome"
+
+    def toggle_lose_weight(self):
+        self.lose_weight = True
+
+    def toggle_gain_muscle(self):
+        self.gain_muscle = True
+
+    def toggle_wake_up(self):
+        self.wake_up = True
+
+    def toggle_plant_food(self):
+        self.plant_food = True
+
+    def toggle_sugar(self):
+        self.sugar = True
+
+
+class WelcomeScreen(Screen):
+    def on_enter(self):
+        Clock.schedule_once(self.change_screen, 3)
+
+    def change_screen(self, scr):
         self.parent.current = "home"
 
 
 class HomeScreen(Screen):
-    pass
+    def enter_meals(self):
+        self.parent.current = "food_entry"
+
+    def enter_exercise(self):
+        self.parent.current = "exercise_entry"
+
+    def enter_sleep(self):
+        self.parent.current = "sleep_entry"
 
 
 class FoodScreen(Screen):
-    pass
+    def food_enter(self):
+        self.meal = self.meal.text
+        self.parent.current = "home"
 
 
 class ExerciseScreen(Screen):
-    pass
+    def exercise_enter(self):
+        self.workout = self.workout.text
+        self.parent.current = "home"
 
 
 class SleepScreen(Screen):
-    pass
+    def sleep_enter(self):
+        self.sleep_start = self.sleep_start.text
+        self.sleep_end = self.sleep_end.text
+        self.parent.current = "home"
 
 
 class Main(MDApp):
